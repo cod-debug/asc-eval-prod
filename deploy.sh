b9c7fcaa65e50e2018e7aa45406c929f0db6cf2d
unknown <roy.duenas@mobilemoney.ph>
2022-11-18 18:46:43 +0800

    update port

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ docker rm asc_eval_fe
 docker rmi asc_eval_fe_v1
 
 docker build -t asc_eval_fe_v1 .
-docker run -d -p 1313:1313 --name asc_eval_fe asc_eval_fe_v1
+docker run -d -p 1315:1315 --name asc_eval_fe asc_eval_fe_v1
 docker image prune -f
 
